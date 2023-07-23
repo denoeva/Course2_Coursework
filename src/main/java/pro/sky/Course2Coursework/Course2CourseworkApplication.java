c855c616f25ffac6b7c85aa52eb2a1a0358ad03d
elena.denoeva <denoeva.elena@skyeng.ru>
2023-07-23 13:08:34 +0300

    Initial commit

---
+++ src/main/java/pro/sky/Course2Coursework/Course2CourseworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2Coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2CourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2CourseworkApplication.class, args);
+	}
+
+}
